8c2399f6c249f3fbd1245738d745e1fff7a0fe7a
liemnt <>
2022-05-19 17:30:41 +0700

    1

--- socket/index.js
+++ socket/index.js
@@ -5,41 +5,65 @@ let users = []
 function SocketIO(server) {
     const io = socketio(server);
     io.on('connect', (socket) => {
-        socket.on('JOIN_CHAT', ({ user_id }, callback) => {
+        socket.on('JOIN_CHAT', async ({ user_id }) => {
             // io.to(user.room).emit('ROOM_DATA', { room: user.room, users: getUsersInRoom(user.room) });
-            callback();
+            
+            if (!users.some(user => user.user_id == user_id)) {
+                let new_user = await User.findOne({ _id: user_id })
+                if (new_user) {
+                    let data =
+                    {   user_id : new_user._id  ,
+                        username : new_user.username , 
+                        full_name : new_user.full_name , 
+                        socket_id: socket.id }
+                    users.push(data)
+                }
+                io.emit('USERS_ROOM', { users });
+                
+            }
         });
-
+        
         socket.on('JOIN_ROOM', async ({ user_id, room_id }, callback) => {
             socket.join(room_id);   
             if (!users.some(user => user._id == user_id)) {
                 let new_user = await User.findOne({ _id: user_id })
                 if (new_user) {
-                    users.push(new_user)
+                    users.push({ username : new_user.username , socket_id: socket.id })
                 }
             }
-            io.to(room_id).emit('USERS_ROOM', { users });
         });
 
+        socket.on('IS_TYPING_CLIENT', async ({ user_id, room_id , is_typing }) => {
+            if (!user_id || !room_id ) return
+            let room = users.find(a => a.user_id == room_id)
+            if(room) io.to(room.socket_id).emit('TYPING_SERVER',{ userId : user_id, roomId : room_id , is_typing  })
+        });
 
-        socket.on('SEND_MESSAGE', async (message, callback) => {
-            const { sender, room_id } = message
-            if (sender) {
-                let new_message = new Message(message);
-                await new_message.save()
-                io.to(room_id).emit('NEW_MESSAGE', new_message);
-
+        socket.on('UPDATE_STATUS_MESSAGE',  ({ data}) => {
+            const { _id,sender , room_id ,status } = data
+            let room = users.find(a => a.user_id == sender)
+            let message_update = {
+                _id, sender , room_id ,status
             }
-            callback();
+            if(room) io.to(room.socket_id).emit('UPDATE_STATUS_MESSAGE_SEVER',{ message_update })
         });
 
-        // socket.on('disconnect', () => {
-        //     const new_users = users.filter(a => a.socket_id != socket.id)
+        socket.on('SEND_MESSAGE', async (message) => {
+            const { sender, room_id , isGroup } = message
+            if (!sender || !room_id ) return
+            if(!isGroup){
+                let new_message = new Message(message);
+                //await new_message.save()
+                let room = users.find(a => a.user_id == room_id)
+                io.to(room.socket_id).to(socket.id).emit('NEW_MESSAGE', new_message);
+            }
+            
+        });
 
-        //     if (new_users) {
-        //         io.emit('USERS_ROOM', {new_users});
-        //     }
-        // })
+        socket.on('disconnect', () => {
+            users = users.filter(a => a.socket_id != socket.id)
+            io.emit('USERS_ROOM', {users});
+        })
     });
 }
 export default SocketIO; 
